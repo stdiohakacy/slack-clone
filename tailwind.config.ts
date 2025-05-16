b153e0b347c1e36d28f652c9ddd4da99f4eff504
Cornal Nguyen <duy.nguyend.tpv@one-line.com>
2025-05-16 19:33:04 +0700

    feat: done ui sign in sign up

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0070f3',
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', 'Arial', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
